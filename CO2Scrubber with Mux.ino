b00d5be31ab02c15d153201ab40158b7aaba8ebd
irosas5103 <120818128+irosas5103@users.noreply.github.com>
2023-02-02 10:16:38 -0600

    Update CO2Scrubber with Mux.ino

--- CO2Scrubber with Mux.ino
+++ CO2Scrubber with Mux.ino
@@ -7,7 +7,7 @@ SCD4x Sensor2; // Output sensor
 
 // Code for selecting the I2C bus.
 void TCA9548A(uint8_t bus){
-  Wire.beginTransmission(0x70);  // TCA9548A address is 0x70
+  Wire.beginTransmission(0x70);  // TCA9548A(mux) address is 0x70
   Wire.write(1 << bus);          // send byte to select bus
   Wire.endTransmission();
 }
